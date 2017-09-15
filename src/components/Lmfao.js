ba6bb7211d3f2805ba6bf6ad6493c6e33549aa3e
Vincent Howard <vincenthoward@Vincents-MacBook-Pro.local>
2017-09-15 10:11:26 -0700

    added lmfao

---
+++ src/components/Lmfao.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Lmfao extends Component {
+  render() {
+    return (
+      <div>
+        Lmfao
+      </div>
+    );
+  }
+}
+
+export default Lmfao;
