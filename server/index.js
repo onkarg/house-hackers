68be9e56413d23d7135f178a0d7cb678121e44bf
Onkar Goswami <onkarg@gmail.com>
2018-01-10 16:26:14 -0700

    C:/Program Files/Git/dashboard get endpoint created with a check to see if req.user exists for success or failure redirect

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const massive = require("massive");
-const session = require("express-session");
-const passport = require("passport");
-const strategy = require("../strategy");
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+const session = require('express-session');
+const passport = require('passport');
+const strategy = require('../strategy');
 const port = process.env.PORT || 3000;
 
-require("dotenv").config();
+require('dotenv').config();
 
-const ctrl = require("./controller/house_controller");
+const ctrl = require('./controller/house_controller');
 
 const app = express();
 
 app.use(
   session({
-    secret: "anything",
+    secret: 'anything',
     resave: false,
     saveUninitialized: false
   })
@@ -39,27 +39,35 @@ passport.deserializeUser(function(obj, done) {
 app.use(bodyParser.json());
 
 massive(process.env.CONNECTION_STRING).then(db => {
-  app.set("db", db);
+  app.set('db', db);
 
   db.init_tables.create_users_seed().then(response => {
-    console.log("User table init");
+    console.log('User table init');
     db.init_tables.create_houses_seed().then(response => {
-      console.log("Houses table init");
+      console.log('Houses table init');
     });
   });
 });
 
 app.get(
-  "/login",
-  passport.authenticate("auth0", {
-    successRedirect: "/dashboard",
-    failureRedirect: "/login",
+  '/login',
+  passport.authenticate('auth0', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/login',
     failureFlash: true
   })
 );
 
-app.get("/api/users", ctrl.getUsers);
-app.get("/api/houses", ctrl.getHouses);
+app.get('/dashboard', (req, res, next) => {
+  if (!req.user ) {
+    res.redirect('/login');
+  } else {
+    res.status(200).send( JSON.stringify( req.user, null, 10 ) );
+  }
+});
+
+app.get('/api/users', ctrl.getUsers);
+app.get('/api/houses', ctrl.getHouses);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
