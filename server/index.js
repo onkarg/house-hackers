921cd65dc101438702d4ffe8cb55f56d13090d0f
Onkar Goswami <onkarg@gmail.com>
2018-01-10 16:22:14 -0700

    C:/Program Files/Git/login get endpoint created for passport.authenticate with success and failure redirects

--- server/index.js
+++ server/index.js
@@ -1,52 +1,66 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const massive = require('massive');
-const session = require('express-session');
-const passport = require('passport');
-const strategy = require('../strategy');
+const express = require("express");
+const bodyParser = require("body-parser");
+const massive = require("massive");
+const session = require("express-session");
+const passport = require("passport");
+const strategy = require("../strategy");
 const port = process.env.PORT || 3000;
 
-require('dotenv').config();
+require("dotenv").config();
 
-const ctrl = require('./controller/house_controller');
+const ctrl = require("./controller/house_controller");
 
 const app = express();
 
-app.use( session({
-  secret: 'anything',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use( passport.initialize() );
-app.use( passport.session() );
-passport.use( strategy );
+app.use(
+  session({
+    secret: "anything",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
 
 passport.serializeUser(function(user, done) {
-  done(null, { id: user.id, display: user.displayName, nickname: user.nickname, email: user.emails[0].value });
+  done(null, {
+    id: user.id,
+    display: user.displayName,
+    nickname: user.nickname,
+    email: user.emails[0].value
+  });
 });
 
 passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 
-massive( process.env.CONNECTION_STRING ).then( db => {
-    app.set('db', db);
-  
+massive(process.env.CONNECTION_STRING).then(db => {
+  app.set("db", db);
 
-    db.init_tables.create_users_seed().then( response => {
-      console.log('User table init');
-      db.init_tables.create_houses_seed().then( response => {
-        console.log('Houses table init');
-      });
+  db.init_tables.create_users_seed().then(response => {
+    console.log("User table init");
+    db.init_tables.create_houses_seed().then(response => {
+      console.log("Houses table init");
     });
   });
+});
 
-app.get('/api/users', ctrl.getUsers);
-app.get('/api/houses', ctrl.getHouses);
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
 
+app.get("/api/users", ctrl.getUsers);
+app.get("/api/houses", ctrl.getHouses);
 
-
-
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
