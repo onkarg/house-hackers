3ad0a0532d3d6535b097dd7e64576d79519e7bbf
Onkar Goswami <onkarg@gmail.com>
2018-01-11 16:59:43 -0700

    routes imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
 import logo from "./logo.png";
+import routes from './routes'
 
 class App extends Component {
   render() {
     return (
-      <div className="">
-        <div class="flexContainer blueBackground">
-          <div class="nav flexItem flexStart">
-            <div>
-              <img src={logo} alt='logo' />
-            </div>
-          </div>
-          <div class="nav flexContainer flexEnd">
-            <div>
-              Contact
-            </div>
-          </div>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        { routes }
       </div>
     );
   }
