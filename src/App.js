7b989a378a9679a4b964e8c4ea11f6730493cf63
Onkar Goswami <onkarg@gmail.com>
2018-01-11 17:02:15 -0700

    HashRouter imported to index.js to configure app to handle routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import logo from "./logo.png";
-import routes from './routes'
+import routes from "./routes";
 
 class App extends Component {
   render() {
@@ -15,7 +15,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        { routes }
+        {routes}
       </div>
     );
   }
